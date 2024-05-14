00aa7154bdc24d0fd7bc13002b43614897b85c85
Parth Dhameliya <parthdhameliya789@gmail.com>
2024-05-14 00:00:55 -0400

    coda-fix

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name="torchret",
     description="",
     long_description="",
-    version = '0.0.2',
+    version = '0.0.3',
     long_description_content_type="text/markdown",
     author="Parth Dhameliya",
     url="https://github.com/parthdhameliya7",
